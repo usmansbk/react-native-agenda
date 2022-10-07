cb18bfa17cf460622f12a10d38e1d4be0e00555e
usmansbk <usmansbk@gmail.com>
2022-10-07 08:09:18 +0100

    setup linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["react", "@typescript-eslint"],
   rules: {},
 };
