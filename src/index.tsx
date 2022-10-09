5abf5fbb6fca94675ea7d895f8753739cd511bbb
usmansbk <usmansbk@gmail.com>
2022-10-09 12:01:45 +0100

    export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import AgendaList from '~components/AgendaList';
 export {Frequency, Weekday} from 'rrule';
 export type {AgendaListProps} from '~components/AgendaList';
+export type {AgendaItem, AgendaSection} from '~types';
 
 export default AgendaList;
