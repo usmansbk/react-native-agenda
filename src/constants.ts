6979e963a64d60e83571f98273ff4af3c29bdc26
usmansbk <usmansbk@gmail.com>
2022-10-09 23:54:51 +0100

    fix typo

--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,10 @@ export const TIME_FORMAT = 'HH:mm';
 export const ITEM_HEIGHT = 40;
 
 export const DAY_FORMATS: DayFormats = {
-  sameDay: '[Today,] dddd, D MMMM',
-  nextDay: '[Tomorrow,], dddd, D MMMM',
+  sameDay: '[Today], dddd, D MMMM',
+  nextDay: '[Tomorrow], dddd, D MMMM',
   nextWeek: 'dddd, D MMMM',
-  lastDay: '[Yesterday,] dddd, D MMMM',
+  lastDay: '[Yesterday], dddd, D MMMM',
   lastWeek: 'dddd, D MMMM',
   sameElse: 'dddd, D MMMM, YYYY',
 };
