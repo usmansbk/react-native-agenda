eeea9d286fc98663980550e6b2ee5ddddd24d1b6
usmansbk <usmansbk@gmail.com>
2022-10-14 09:03:27 +0100

    export day header

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import AgendaList from '~components/AgendaList';
 export {Frequency, Weekday} from 'rrule';
 export type {AgendaListProps} from '~components/AgendaList';
+export {default as DayHeader} from '~components/DayHeader';
 export type {AgendaItem, AgendaSection} from '~types';
 
 export default AgendaList;
